61ef40dcc3342fc9f1aa21f5670bb0899c67211c
PereHDZ <pere.hdz.salleras@gmail.com>
2025-08-12 19:40:28 +0200

    Populate first 50 abilities

---
+++ pokedex/api/data/populates/abilities/001-0050/abilities26_50.ts
@@ -0,0 +1,132 @@
+export const abilities26_50 = [
+    {
+        id: 26,
+        name: 'Levitate',
+        description: `By floating in the air, the Pokémon receives full immunity to all Ground-type moves.`
+    },
+    {
+        id: 27,
+        name: 'Effect Spore',
+        description: 'Contact with the Pokémon may inflict poison, sleep, or paralysis on the attacker (10% in gen III, 30% onward).'
+    },
+    {
+        id: 28,
+        name: 'Synchronize',
+        description: `If the Pokémon is burned, paralyzed, or poisoned by another Pokémon, 
+        that Pokémon will be inflicted with the same status condition.`
+    },
+    {
+        id: 29,
+        name: 'Clear Body',
+        description: "Prevents other Pokémon's moves or Abilities from lowering the Pokémon's stats."
+    },
+    {
+        id: 30,
+        name: 'Natural Cure',
+        description: `The Pokémon's status conditions are cured when it switches out.`
+    },
+    {
+        id: 31,
+        name: 'Lightning Rod',
+        description: `The Pokémon draws in all Electric-type moves. 
+        Instead of taking damage from them, its Sp. Atk stat is boosted (Only from gen V onwards).`
+    },
+    {
+        id: 32,
+        name: 'Serene Grace',
+        description: "Raises the likelihood of additional effects occurring when the Pokémon uses its moves."
+    },
+    {
+        id: 33,
+        name: 'Swift Swim',
+        description: "Doubles the Pokémon's Speed stat in rain."
+    },
+    {
+        id: 34,
+        name: 'Chlorophyll',
+        description: `Doubles the Pokémon's Speed stat in harsh sunlight.`
+    },
+    {
+        id: 35,
+        name: 'Illuminate',
+        description: `By illuminating its surroundings, 
+        the Pokémon prevents its accuracy from being lowered and increases the odds of finding wild pókemon by 100%`
+    },
+    {
+        id: 36,
+        name: 'Trace',
+        description: "When it enters a battle, the Pokémon copies an opposing Pokémon's Ability."
+    },
+    {
+        id: 37,
+        name: 'Huge Power',
+        description: "Doubles the Pokémon's Attack stat."
+    },
+    {
+        id: 38,
+        name: 'Poison Point',
+        description: 'Contact with the Pokémon may poison the attacker (30% chance).'
+    },
+    {
+        id: 39,
+        name: 'Inner Focus',
+        description: `The Pokémon's intense focus prevents it from flinching or being affected by Intimidate 
+        (only from gen VIII onwards).`
+    },
+    {
+        id: 40,
+        name: 'Magma Armor',
+        description: "The Pokémon's hot magma coating prevents it from being frozen."
+    },
+    {
+        id: 41,
+        name: 'Water Veil',
+        description: "The Pokémon's water veil prevents it from being burned."
+    },
+    {
+        id: 42,
+        name: 'Magnet Pull',
+        description: `Prevents Steel-type Pokémon from fleeing by pulling them in with magnetism. 
+        It also increases the odds of finding Steel-type pókemon by 50%`
+    },
+    {
+        id: 43,
+        name: 'Soundproof',
+        description: `Soundproofing gives the Pokémon full immunity to all sound-based moves.`
+    },
+    {
+        id: 44,
+        name: 'Rain Dish',
+        description: 'The Pokémon gradually regains HP (1/16 of its HP) in rain.'
+    },
+    {
+        id: 45,
+        name: 'Sand Stream',
+        description: 'The Pokémon summons a sandstorm when it enters a battle.'
+    },
+    {
+        id: 46,
+        name: 'Pressure',
+        description: `Puts other Pokémon under pressure, causing them to expend double the PP to use their moves.`
+    },
+    {
+        id: 47,
+        name: 'Thick Fat',
+        description: `The Pokémon is protected by a layer of thick fat, which halves the damage taken from Fire- and Ice-type moves.`
+    },
+    {
+        id: 48,
+        name: 'Early Bird',
+        description: "The Pokémon awakens from sleep twice as fast as other Pokémon."
+    },
+    {
+        id: 49,
+        name: 'Flame Body',
+        description: `Contact with the Pokémon may burn the attacker (30% chance). It also halves the time needed to hatch eggs.`
+    },
+    {
+        id: 50,
+        name: 'Run Away',
+        description: 'Enables a sure getaway from wild Pokémon.'
+    }
+]   
--- pokedex/api/data/populates/abilities/index.ts
+++ pokedex/api/data/populates/abilities/index.ts
@@ -1,5 +1,7 @@
 import { abilities1_25 } from './001-0050/abilities1_25'
+import { abilities26_50 } from './001-0050/abilities26_50'
 
 export default [
-    ...abilities1_25
+    ...abilities1_25,
+    ...abilities26_50
 ]
