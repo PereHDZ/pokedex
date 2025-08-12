eba96c8c3c9d88aa69c7daff54abb182ba39a36d
PereHDZ <pere.hdz.salleras@gmail.com>
2025-08-12 19:57:54 +0200

    Populate first 75  abilities

---
+++ pokedex/api/data/populates/abilities/001-0050/abilities51_75.ts
@@ -0,0 +1,129 @@
+export const abilities51_75 = [
+    {
+        id: 51,
+        name: 'Keen Eye',
+        description: `The Pokémon's keen eyes prevent its accuracy from being lowered. 
+        It also ignores the target's stages in evasion (only from gen VI onwards).`
+    },
+    {
+        id: 52,
+        name: 'Hyper Cutter',
+        description: "The Pokémon's prized, mighty pincers prevent other Pokémon from lowering its Attack stat."
+    },
+    {
+        id: 53,
+        name: 'Pickup',
+        description: `The Pokémon may pick up an item another Pokémon used during a battle (only from gen V onward). 
+        It may pick up items outside of battle, too`
+    },
+    {
+        id: 54,
+        name: 'Truant',
+        description: "Each time the Pokémon uses a move, it spends the next turn loafing around."
+    },
+    {
+        id: 55,
+        name: 'Hustle',
+        description: `Boosts the Pokémon's Attack stat but lowers its accuracy.`
+    },
+    {
+        id: 56,
+        name: 'Cute Charm',
+        description: `The Pokémon may infatuate attackers that make direct contact with it (30% chance).`
+    },
+    {
+        id: 57,
+        name: 'Plus',
+        description: "Boosts the Sp. Atk stat of the Pokémon if an ally with the Plus or Minus Ability is also in battle."
+    },
+    {
+        id: 58,
+        name: 'Minus',
+        description: "Boosts the Sp. Atk stat of the Pokémon if an ally with the Plus or Minus Ability is also in battle."
+    },
+    {
+        id: 59,
+        name: 'Forecast',
+        description: `The Pokémon transforms with the weather to change its type to Water, Fire, or Ice.`
+    },
+    {
+        id: 60,
+        name: 'Sticky Hold',
+        description: `The Pokémon's held items cling to its sticky body and cannot be removed by other Pokémon.`
+    },
+    {
+        id: 61,
+        name: 'Shed Skin',
+        description: "The Pokémon may cure its own status conditions by shedding its skin (30% chance)."
+    },
+    {
+        id: 62,
+        name: 'Guts',
+        description: "It's so gutsy that having a status condition boosts the Pokémon's Attack stat by 50%."
+    },
+    {
+        id: 63,
+        name: 'Marvel Scale',
+        description: "The Pokémon's marvelous scales boost its Defense stat by 50% if it has a status condition."
+    },
+    {
+        id: 64,
+        name: 'Liquid Ooze',
+        description: `The strong stench of the Pokémon's oozed liquid damages attackers that use HP-draining moves.`
+    },
+    {
+        id: 65,
+        name: 'Overgrow',
+        description: "Powers up Grass-type moves by 50% when the Pokémon's HP is below 30%."
+    },
+    {
+        id: 66,
+        name: 'Blaze',
+        description: "Powers up Fire-type moves by 50% when the Pokémon's HP is below 30%."
+    },
+    {
+        id: 67,
+        name: 'Torrent',
+        description: `Powers up Water-type moves by 50% when the Pokémon's HP is below 30%.`
+    },
+    {
+        id: 68,
+        name: 'Swarm',
+        description: `Powers up Bug-type moves by 50% when the Pokémon's HP is below 30%.`
+    },
+    {
+        id: 69,
+        name: 'Rock Head',
+        description: 'Protects the Pokémon from recoil damage.'
+    },
+    {
+        id: 70,
+        name: 'Drought',
+        description: 'Turns the sunlight harsh when the Pokémon enters a battle.'
+    },
+    {
+        id: 71,
+        name: 'Arena Trap',
+        description: `Prevents opposing Pokémon from fleeing from battle.`
+    },
+    {
+        id: 72,
+        name: 'Vital Spirit',
+        description: "The Pokémon is full of vitality, and that prevents it from falling asleep."
+    },
+    {
+        id: 73,
+        name: 'White Smoke',
+        description: `The Pokémon is protected by its white smoke, which prevents other Pokémon from lowering its stats.`
+    },
+    {
+        id: 74,
+        name: 'Pure Power',
+        description: 'Using its pure power, the Pokémon doubles its Attack stat.'
+    },
+    {
+        id: 75,
+        name: 'Shell Armor',
+        description: `A hard shell protects the Pokémon from critical hits.`
+    }
+]   
--- pokedex/api/data/populates/abilities/index.ts
+++ pokedex/api/data/populates/abilities/index.ts
@@ -1,7 +1,9 @@
 import { abilities1_25 } from './001-0050/abilities1_25'
 import { abilities26_50 } from './001-0050/abilities26_50'
+import { abilities51_75 } from './001-0050/abilities51_75'
 
 export default [
     ...abilities1_25,
-    ...abilities26_50
+    ...abilities26_50,
+    ...abilities51_75
 ]
