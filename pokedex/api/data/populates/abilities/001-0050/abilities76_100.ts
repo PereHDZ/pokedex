5179bc44d4527e585acceb053564d1b4b6ef6f1f
PereHDZ <pere.hdz.salleras@gmail.com>
2025-08-12 22:20:35 +0200

    Populate first 100  abilities

---
+++ pokedex/api/data/populates/abilities/001-0050/abilities76_100.ts
@@ -0,0 +1,131 @@
+export const abilities76_100 = [
+    {
+        id: 76,
+        name: 'Air Lock',
+        description: `Eliminates the effects of weather.`
+    },
+    {
+        id: 77,
+        name: 'Tangled Feet',
+        description: "Boosts the Pokémon's evasiveness if it is confused."
+    },
+    {
+        id: 78,
+        name: 'Motor Drive',
+        description: `The Pokémon takes no damage when hit by Electric-type moves. Instead, its Speed stat is boosted.`
+    },
+    {
+        id: 79,
+        name: 'Rivalry',
+        description: `The Pokémon's competitive spirit makes it deal more damage to Pokémon of the same gender (+25%), 
+        but less damage to Pokémon of the opposite gender.`
+    },
+    {
+        id: 80,
+        name: 'Steadfast',
+        description: `The Pokémon's determination boosts its Speed stat every time it flinches.`
+    },
+    {
+        id: 81,
+        name: 'Snow Cloak',
+        description: `Boosts the Pokémon's evasiveness in snow (by 4/5).`
+    },
+    {
+        id: 82,
+        name: 'Gluttony',
+        description: `If the Pokémon is holding a Berry to be eaten when its HP is low, 
+        it will instead eat the Berry when its HP drops to half or less.`
+    },
+    {
+        id: 83,
+        name: 'Anger Point',
+        description: "The Pokémon is angered when it takes a critical hit, and that maxes its Attack stat."
+    },
+    {
+        id: 84,
+        name: 'Unburden',
+        description: `Boosts the Speed stat if the Pokémon's held item is used or lost.`
+    },
+    {
+        id: 85,
+        name: 'Heatproof',
+        description: `The Pokémon's heatproof body halves the damage taken from Fire-type moves.`
+    },
+    {
+        id: 86,
+        name: 'Simple',
+        description: "Doubles the effects of the Pokémon's stat changes."
+    },
+    {
+        id: 87,
+        name: 'Dry Skin',
+        description: `Restores the Pokémon's HP in rain (1/8 of its HP) or when it is hit by Water-type moves. 
+        Reduces HP in harsh sunlight, and increases the damage received from Fire-type moves by 25%.`
+    },
+    {
+        id: 88,
+        name: 'Download',
+        description: `	The Pokémon compares an opposing Pokémon's Defense and Sp. Def stats before raising its own Attack or Sp. Atk
+         stat — whichever will be more effective.`
+    },
+    {
+        id: 89,
+        name: 'Iron Fist',
+        description: `Powers up punching moves by 20%.`
+    },
+    {
+        id: 90,
+        name: 'Poison Heal',
+        description: "If poisoned, the Pokémon has its HP restored instead of taking damage."
+    },
+    {
+        id: 91,
+        name: 'Adaptability',
+        description: "Powers up moves of the same type as the Pokémon from x1.5 to x2."
+    },
+    {
+        id: 92,
+        name: 'Skill Link',
+        description: `Maximizes the number of times multistrike moves hit.`
+    },
+    {
+        id: 93,
+        name: 'Hydration',
+        description: `Cures the Pokémon's status conditions in rain.`
+    },
+    {
+        id: 94,
+        name: 'Solar Power',
+        description: "In harsh sunlight, the Pokémon's Sp. Atk stat is boosted by 1.5, but its HP decreases every turn by 1/8."
+    },
+    {
+        id: 95,
+        name: 'Quick Feet',
+        description: 'Boosts the Speed stat if the Pokémon has a status condition.  '
+    },
+    {
+        id: 96,
+        name: 'Normalize',
+        description: `All the Pokémon's moves become Normal type. The power of those moves is boosted by 20% from gen VII onwards.`
+    },
+    {
+        id: 97,
+        name: 'Sniper',
+        description: "If the Pokémon's attack lands a critical hit, the attack is powered up even further (+50%)."
+    },
+    {
+        id: 98,
+        name: 'Magic Guard',
+        description: `The Pokémon only takes damage from attacks.`
+    },
+    {
+        id: 99,
+        name: 'No Guard',
+        description: 'The Pokémon employs no-guard tactics to ensure incoming and outgoing attacks always land.'
+    },
+    {
+        id: 100,
+        name: 'Stall',
+        description: `The Pokémon is always the last to use its moves.`
+    }
+]   
