aeb7d07febf7ddf1fefaf193be9231f75d1e4d5d
PereHDZ <pere.hdz.salleras@gmail.com>
2025-08-13 16:43:58 +0200

    Populate first 150  abilities

---
+++ pokedex/api/data/populates/abilities/101-200/abilities126_150.ts
@@ -0,0 +1,129 @@
+export const abilities126_150 = [
+    {
+        id: 126,
+        name: 'Contrary',
+        description: `Reverses any stat changes affecting the Pokémon so that attempts to boost its stats instead lower them 
+        — and attempts to lower its stats will boost them.`
+    },
+    {
+        id: 127,
+        name: 'Unnerve',
+        description: "Unnerves opposing Pokémon and makes them unable to eat Berries."
+    },
+    {
+        id: 128,
+        name: 'Defiant',
+        description: `If the Pokémon has any stat lowered by an opposing Pokémon, its Attack stat will be boosted sharply.`
+    },
+    {
+        id: 129,
+        name: 'Defeatist',
+        description: `Halves the Pokémon's Attack and Sp. Atk stats when its HP drops to half or less.`
+    },
+    {
+        id: 130,
+        name: 'Cursed Body',
+        description: `Has a 30% chance to disable a move that has dealt damage to the Pokémon.`
+    },
+    {
+        id: 131,
+        name: 'Healer',
+        description: `Has a 30% chance to cure the status conditions of the Pokémon's allies.`
+    },
+    {
+        id: 132,
+        name: 'Friend Guard',
+        description: `Reduces damage dealt to allies by 25%.`
+    },
+    {
+        id: 133,
+        name: 'Weak Armor',
+        description: `The Pokémon's Defense stat is lowered when it takes damage from physical moves, 
+        but its Speed stat is sharply boosted.`
+    },
+    {
+        id: 134,
+        name: 'Heavy Metal',
+        description: `Doubles the Pokémon's weight.`
+    },
+    {
+        id: 135,
+        name: 'Light Metal',
+        description: `Halves the Pokémon's weight.`
+    },
+    {
+        id: 136,
+        name: 'Multiscale',
+        description: "Reduces the amount of damage the Pokémon takes by half while its HP is full."
+    },
+    {
+        id: 137,
+        name: 'Toxic Boost',
+        description: `Powers up physical moves when the Pokémon is poisoned by 50%.`
+    },
+    {
+        id: 138,
+        name: 'Flare Boost',
+        description: `TPowers up physical moves when the Pokémon is burned by 50%.`
+    },
+    {
+        id: 139,
+        name: 'Harvest',
+        description: `May create another Berry after one is used (50% chance).`
+    },
+    {
+        id: 140,
+        name: 'Telepathy',
+        description: "The Pokémon anticipates and dodges the attacks of its allies."
+    },
+    {
+        id: 141,
+        name: 'Moody',
+        description: "Every turn, one of the Pokémon's stats will be boosted sharply, but another stat will be lowered."
+    },
+    {
+        id: 142,
+        name: 'Overcoat',
+        description: `The Pokémon takes no damage from sandstorms. It is also protected from the effects of powders and spores.`
+    },
+    {
+        id: 143,
+        name: 'Poison Touch',
+        description: `May poison a target when the Pokémon makes contact (30% chance).`
+    },
+    {
+        id: 144,
+        name: 'Regenerator',
+        description: "The Pokémon has a little (33%) of its HP restored when withdrawn from battle."
+    },
+    {
+        id: 145,
+        name: 'Big Pecks',
+        description: 'Prevents the Pokémon from having its Defense stat lowered.'
+    },
+    {
+        id: 146,
+        name: 'Sand Rush',
+        description: `Boosts the Pokémon's Speed stat in a sandstorm.`
+    },
+    {
+        id: 147,
+        name: 'Wonder Skin',
+        description: "Makes status moves 50% more likely to miss the Pokémon."
+    },
+    {
+        id: 148,
+        name: 'Analytic',
+        description: `Boosts the power of the Pokémon's move by 30% if it is the last to act that turn.`
+    },
+    {
+        id: 149,
+        name: 'Illusion',
+        description: `The Pokémon fools opponents by entering battle disguised as the last Pokémon in its Trainer's party.`
+    },
+    {
+        id: 150,
+        name: 'Imposter',
+        description: `The Pokémon transforms itself into the Pokémon it's facing.`
+    }
+]   
--- pokedex/api/data/populates/abilities/index.ts
+++ pokedex/api/data/populates/abilities/index.ts
@@ -3,11 +3,13 @@ import { abilities26_50 } from './001-100/abilities26_50'
 import { abilities51_75 } from './001-100/abilities51_75'
 import { abilities76_100 } from './001-100/abilities76_100'
 import { abilities101_125 } from './101-200/abilities101-125'
+import { abilities126_150 } from './101-200/abilities126_150'
 
 export default [
     ...abilities1_25,
     ...abilities26_50,
     ...abilities51_75,
     ...abilities76_100,
-    ...abilities101_125
+    ...abilities101_125,
+    ...abilities126_150
 ]
