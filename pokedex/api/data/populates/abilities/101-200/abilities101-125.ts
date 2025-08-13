89fbbc559dc0599465ce74898de2725fb0e92165
PereHDZ <pere.hdz.salleras@gmail.com>
2025-08-13 12:25:43 +0200

    Populate first 125  abilities

---
+++ pokedex/api/data/populates/abilities/101-200/abilities101-125.ts
@@ -0,0 +1,129 @@
+export const abilities101_125 = [
+    {
+        id: 101,
+        name: 'Technician',
+        description: `Powers up weak moves (60 base power or less) so the Pokémon can deal more damage with them (+50%).`
+    },
+    {
+        id: 102,
+        name: 'Leaf Guard',
+        description: "Prevents status conditions in harsh sunlight."
+    },
+    {
+        id: 103,
+        name: 'Klutz',
+        description: `The Pokémon can't use any held items.`
+    },
+    {
+        id: 104,
+        name: 'Mold Breaker',
+        description: `The Pokémon's moves are unimpeded by the Ability of the target.`
+    },
+    {
+        id: 105,
+        name: 'Super Luck',
+        description: `The Pokémon is so lucky that the critical-hit ratios of its moves are boosted.`
+    },
+    {
+        id: 106,
+        name: 'Aftermath',
+        description: `Damages the attacker (25% of its HP) if it knocks out the Pokémon with a move that makes direct contact.`
+    },
+    {
+        id: 107,
+        name: 'Anticipation',
+        description: `The Pokémon can sense an opposing Pokémon's dangerous moves.`
+    },
+    {
+        id: 108,
+        name: 'Forewarn',
+        description: "When it enters a battle, the Pokémon can tell one of the moves an opposing Pokémon has."
+    },
+    {
+        id: 109,
+        name: 'Unaware',
+        description: `When attacking, the Pokémon ignores the target's stat changes.`
+    },
+    {
+        id: 110,
+        name: 'Tinted Lense',
+        description: `The Pokémon can use "not very effective" moves to deal regular damage.`
+    },
+    {
+        id: 111,
+        name: 'Filter',
+        description: "Reduces the power of supereffective attacks that hit the Pokémon."
+    },
+    {
+        id: 112,
+        name: 'Slow Start',
+        description: `For five turns, the Pokémon's Attack and Speed stats are halved.`
+    },
+    {
+        id: 113,
+        name: 'Scrappy',
+        description: `The Pokémon can hit Ghost-type Pokémon with Normal- and Fighting-type moves. It is also unaffected by Intimidate.
+        From gen VIII onward, they are also unaffected by Intimidate`
+    },
+    {
+        id: 114,
+        name: 'Storm Drain',
+        description: `The Pokémon draws in all Water-type moves. From gen V onwards, instead of taking damage from them, 
+        its Sp. Atk stat is boosted.`
+    },
+    {
+        id: 115,
+        name: 'Ice Body',
+        description: "The Pokémon gradually regains 1/16 of its HP in snow."
+    },
+    {
+        id: 116,
+        name: 'Solid Rock',
+        description: "Reduces the power of supereffective attacks that hit the Pokémon."
+    },
+    {
+        id: 117,
+        name: 'Snow Warning',
+        description: `The Pokémon makes it snow when it enters a battle.`
+    },
+    {
+        id: 118,
+        name: 'Honey Gather',
+        description: `The Pokémon may gather Honey after a battle.`
+    },
+    {
+        id: 119,
+        name: 'Frisk',
+        description: "When it enters a battle, the Pokémon can check an opposing Pokémon's held item."
+    },
+    {
+        id: 120,
+        name: 'Reckless',
+        description: 'Powers up moves that have recoil damage by 20%.'
+    },
+    {
+        id: 121,
+        name: 'Multitype',
+        description: `Changes the Pokémon's type to match the plate it holds.`
+    },
+    {
+        id: 122,
+        name: 'Flower Gift',
+        description: "Boosts the Attack and Sp. Def stats of the Pokémon and its allies in harsh sunlight by 50%."
+    },
+    {
+        id: 123,
+        name: 'Bad Dreams',
+        description: `Damages opposing Pokémon that are asleep (1/8 of its HP).`
+    },
+    {
+        id: 124,
+        name: 'Pickpoket',
+        description: 'The Pokémon steals the held item from attackers that make direct contact with it.'
+    },
+    {
+        id: 125,
+        name: 'Sheer Force',
+        description: `Removes any additional effects from the Pokémon's moves, but increases the moves' power by 30%.`
+    }
+]   
--- pokedex/api/data/populates/abilities/index.ts
+++ pokedex/api/data/populates/abilities/index.ts
@@ -1,11 +1,13 @@
-import { abilities1_25 } from './001-0050/abilities1_25'
-import { abilities26_50 } from './001-0050/abilities26_50'
-import { abilities51_75 } from './001-0050/abilities51_75'
-import { abilities76_100 } from './001-0050/abilities76_100'
+import { abilities1_25 } from './001-100/abilities1_25'
+import { abilities26_50 } from './001-100/abilities26_50'
+import { abilities51_75 } from './001-100/abilities51_75'
+import { abilities76_100 } from './001-100/abilities76_100'
+import { abilities101_125 } from './101-200/abilities101-125'
 
 export default [
     ...abilities1_25,
     ...abilities26_50,
     ...abilities51_75,
-    ...abilities76_100
+    ...abilities76_100,
+    ...abilities101_125
 ]
